d3a34f49fea657e4fd246cbc0d4032d04ffd9da8
jeevankumarpekala <jk8187859506@gmail.com>
2024-04-13 10:37:50 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Hello from "./components/index";
+//https://github.com/jeevankumarpekala/hello.git
+import img from "../src/images/img.jpg";
+function Helloworld() {
+  return <img src={img} style={{ width: "400 px", height: "400px" }} />;
+}
+function App() {
+  return (
+    <div className="App">
+      <>
+        <Helloworld />
+      </>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import img from "../images/img.jpg";
+function Hello() {
+  return <img src={img} style={{ width: "200 px", height: "200px" }} />;
+}
+export default Hello;

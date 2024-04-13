67a1d4da31383efa25c3e8b0bc51e9fa473745f6
jeevankumarpekala <jk8187859506@gmail.com>
2024-04-13 10:39:57 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import Hello from "./components/index";
 //https://github.com/jeevankumarpekala/hello.git
+//https://github.com/jeevankumarpekala/myapp.git
 import img from "../src/images/img.jpg";
 function Helloworld() {
   return <img src={img} style={{ width: "400 px", height: "400px" }} />;
